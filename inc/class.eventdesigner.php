2009e36f970bcfe8cb302c9c6ab9268fafb8a18c
Nikolay.korbut <nick.korbut@gmail.com>
2014-08-08 18:45:16 +0300

    add new class

---
+++ inc/class.eventdesigner.php
@@ -0,0 +1,17 @@
+<?php
+
+class eventDesigner {
+ 
+    var $version = "Event Designer version 0.0.0.2";
+ 
+    function eventDesigner(){
+       $this->version = "Event Designer version 0.0.0.3";
+    }
+    
+    function getVersion(){
+     return $this->version;   
+    }
+    
+    
+}
+?>
